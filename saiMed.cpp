28e077505d54709d0b16ea7a853c45fc2da000df
Sai Medavarapu <saivaibhav90@gmail.com>
2019-02-18 18:09:48 -0600

    index values for highest requested elements.

--- saiMed.cpp
+++ saiMed.cpp
@@ -19,29 +19,24 @@ ostream& bold_off(std::ostream& os)
 
 int main()
 {
-    int noOfFloors , elevatorSize = 10,  initialLoc= 5, noOfReq[10], toFloor[10], numbOfReq;
-    int *cnt, *floors;
-    float time_travel[5], totalTime = 0;
+    int noOfFloors , elevatorSize = 10,  initialLoc= 5, noOfReq[100000], toFloor[100000], numbOfReq;
+    int *cnt, *floors, *priorityFloorIndex ;
+    float time_travel[100000], totalTime = 0;
     float timeToMove;
-    /*
-        a. Your code should randomly generate a number of requests.
-        b. Design and implement three different algorithms to simulate the elevator movement in order to
-        response to the requests.
-        c. For each algorithm, you should log the order in which the requests have been served, the time
-        it takes to serve each request, and total time it takes to serve all requests.
-    */
+   
+
    cout<<"The total number of floors.\n";
    cin>>noOfFloors;
    floors = new int[noOfFloors];
-  cout<<"Elevator Size?\n";
-     cin>> elevatorSize;
+ // cout<<"Elevator Size?\n";
+   //  cin>> elevatorSize;
 cout<<"How many number of requests"<<endl;
 cin>>numbOfReq;
      cnt = new int[numbOfReq];
      cout<<"What is The time it takes to move between two floors?(in seconds)\n";
      cin>> timeToMove;
-cout<<"What is the initial location of the elevator?\n";
-    cin>>initialLoc;
+//cout<<"What is the initial location of the elevator?\n";
+  //  cin>>initialLoc;
 
 cout<<"------------Generated Floor requests are---------------\n";
      
@@ -76,9 +71,9 @@ cout<<bold_on<<"Total Time to travel in seconds is : "<<totalTime<<bold_off<<end
 
 
 
-  cout<<"------------------Algorithm 2 - Go to highest first and pick on the go -------------\n";  
+  cout<<"------------------Algorithm 2 - Check the floor with high number of requests -------------\n";  
   //Algorithm2
-
+//This array stores requests from each floor.
 for (int i=0;i<numbOfReq;i++) {
         floors[i] = 0;
        for (int j=0;j<numbOfReq;j++) { 
@@ -88,31 +83,40 @@ for (int i=0;i<numbOfReq;i++) {
       }
        }
 } 
-      
+//Finding the most number of req from which floor?
+   int k=0; 
    for(int i = 0; i < numbOfReq; i++)
+  {
+  cout<< "At floor "<<bold_on<<k++<<" There are "<<floors[i]<<bold_off<<" requests \n";
+  }   
+
+
+//Finding the index of the highest repetetive element - Which lets you find busiest floor.
+int priorityFloor = max_element(floors, floors + numbOfReq) - floors;
+cout<<endl;
+  cout << "Floor which has more number of people is : "
+       << priorityFloor << endl;
+
+// Implementing search to find the index of priority floor 
+
+      priorityFloorIndex = new int[numbOfReq];//Array to hold index values of repetitive element.
+      int v= 0; //index variable for priorityFloorIndex
+      for(int i=0;i<numbOfReq;i++)
+        {
+          if(noOfReq[i] == priorityFloor)
+          { 
+            priorityFloorIndex[v] = i;
+            v++;
+          }
+        }
+//Printing out the indexes of the repetetive values.
+
+for(int i=0;i<v;i++)
 {
-  cout<< floors[i];
-}   
-       
-for(int i = 0; i < numbOfReq; i++)
-{
-  //cout<< cnt[i];
+  cout<<"Index values are : "<< priorityFloorIndex[i]<<"  "<<endl;
 }
+//Now that we have index values, lets serve the requests from that floor.
 
-
-/*int largest = cnt[0];
-int highRequests = 0;
-   for(int i = 1;i < numbOfReq; i++) {
-      
-      if(largest <=cnt[i])
-         {
-         largest = cnt[i];
-          highRequests = noOfReq[i]; 
-          //cout<<"highRequests is :"<< highRequests<<endl;
-          } 
-   }
-
-cout<<"At floor"<<highRequests<<"there are more number of requests"<<endl;*/
 
 return 0;
 }
